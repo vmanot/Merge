1547eb93cc209b57f70ca478654882dce5e92657
Vatsal Manot <vatsal.manot@yahoo.com>
2020-07-12 20:54:50 +0530

    Update package

--- Sources/Intermodular/Helpers/Combine/opaque_VoidSender.swift
+++ Sources/Intermodular/Helpers/Combine/opaque_VoidSender.swift
@@ -9,7 +9,7 @@ public protocol opaque_VoidSender {
     func send()
 }
 
-// MARK: - Concrete Implementations -
+// MARK: - Auxiliary Implementation -
 
 extension CurrentValueSubject: opaque_VoidSender where Output == Void {
     
