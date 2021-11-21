cdcafa231ce9962091a16e205c8de92704acec55
Vatsal Manot <vatsal.manot@yahoo.com>
2021-11-21 19:35:24 +0530

    Update package

--- Sources/Intermodular/Helpers/Swift/Optional.UnwrapPublisher.swift
+++ Sources/Intermodular/Helpers/Swift/Optional.UnwrapPublisher.swift
@@ -8,7 +8,7 @@ import Swallow
 extension Optional {
     public typealias UnwrapPublisher = Either<Result<Wrapped, Optional<Wrapped>.UnwrappingError>.Publisher, Combine.Fail<Wrapped, UnwrappingError>>
     
-    public var unwrapPublisher: UnwrapPublisher{
+    public var unwrapPublisher: UnwrapPublisher {
         guard let wrappedValue = self else {
             return .right(Fail(error: .unexpectedlyFoundNil))
         }
