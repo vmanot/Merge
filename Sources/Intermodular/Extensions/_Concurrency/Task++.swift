d42451f5e3505c554c4a79c353bd6309a4d0c55c
Vatsal Manot <vatsal.manot@yahoo.com>
2022-05-03 04:34:39 +0530

    Fix Task.publisher(priority:)

--- Sources/Intermodular/Extensions/_Concurrency/Task++.swift
+++ Sources/Intermodular/Extensions/_Concurrency/Task++.swift
@@ -2,6 +2,7 @@
 // Copyright (c) Vatsal Manot
 //
 
+import Combine
 import Dispatch
 import Swift
 import SwiftUI
@@ -11,23 +12,19 @@ extension Task {
     public func publisher(
         priority: TaskPriority? = nil
     ) -> AnySingleOutputPublisher<Success, Failure> {
-        let subject = PassthroughSubject<Success, Failure>()
-        
-        let task = Task<Void, Never>.detached(priority: priority) {
-            switch await result {
-                case _ where Task<Never, Never>.isCancelled:
-                    subject.send(completion: .finished)
-                case .success(let value):
-                    subject.send(value)
-                    subject.send(completion: .finished)
-                case .failure(let error):
-                    subject.send(completion: .failure(error))
+        Future.async(priority: priority) { () -> Result<Success, Failure> in
+            await self.result
+        }
+        .flatMap { (result: Result<Success, Failure>) -> AnyPublisher<Success, Failure> in
+            switch result {
+                case .success(let success):
+                    return Just(success).setFailureType(to: Failure.self).eraseToAnyPublisher()
+                case .failure(let failure):
+                    return Fail(error: failure).eraseToAnyPublisher()
             }
         }
-        
-        return subject
-            .handleEvents(receiveCancel: task.cancel)
-            ._unsafe_eraseToAnySingleOutputPublisher()
+        .handleEvents(receiveCancel: self.cancel)
+        ._unsafe_eraseToAnySingleOutputPublisher()
     }
     
     /// Block the current thread and wait for the value.
