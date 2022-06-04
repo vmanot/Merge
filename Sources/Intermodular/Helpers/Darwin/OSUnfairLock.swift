68030b784cc8248e573fdb604e410d7e9561e619
Vatsal Manot <vatsal.manot@yahoo.com>
2022-06-04 18:07:24 +0530

    Update package

--- Sources/Intermodular/Helpers/Darwin/OSUnfairLock.swift
+++ Sources/Intermodular/Helpers/Darwin/OSUnfairLock.swift
@@ -7,21 +7,25 @@ import Swallow
 
 /// An `os_unfair_lock` wrapper.
 public final class OSUnfairLock: Initiable, Sendable, TestableLock {
-    private let base: os_unfair_lock_t
+    @usableFromInline
+    let base: os_unfair_lock_t
     
     public init() {
         base = .allocate(capacity: 1)
         base.initialize(to: os_unfair_lock())
     }
-    
+
+    @inlinable
     public func acquireOrBlock() {
         os_unfair_lock_lock(base)
     }
-    
+
+    @inlinable
     public func acquireOrFail() throws {
         try os_unfair_lock_trylock(base).throw(if: ==false)
     }
-    
+
+    @inlinable
     public func relinquish() {
         os_unfair_lock_unlock(base)
     }
