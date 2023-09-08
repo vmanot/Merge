4052eca666dec6ae5284c310b7c9e696645253fa
Vatsal Manot <vatsal.manot@yahoo.com>
2023-09-08 03:27:15 -0400

    Update package

--- Sources/Intramodular/Utilities/_MaybeAsyncProtocol.swift
+++ Sources/Intramodular/Utilities/_MaybeAsyncProtocol.swift
@@ -4,6 +4,7 @@
 
 import Swift
 
+/// Utility protocol for things that might require preheating/one-off resource resolution and are then synchronous to use.
 public protocol _MaybeAsyncProtocol {
     var _isKnownAsync: Bool? { get }
     
@@ -16,20 +17,34 @@ extension _MaybeAsyncProtocol {
     public var _isKnownAsync: Bool? {
         nil
     }
+    
+    public func _resolveToNonAsync() async throws -> Self {
+        throw Never.Reason.unimplemented
+    }
 }
 
 // MARK: - Supplementary
 
 public func _resolveMaybeAsync<T>(_ x: T) async throws -> T {
-    if let x = x as? _MaybeAsyncProtocol {
-        return try await x._resolveToNonAsync() as! T
-    } else {
-        return x
+    do {
+        if let x = x as? _MaybeAsyncProtocol {
+            return try await x._resolveToNonAsync() as! T
+        } else {
+            return x
+        }
+    } catch {
+        switch error {
+            case _MaybeAsyncProtocolError.noResolutionImplementation:
+                return x
+            default:
+                throw error
+        }
     }
 }
 
 // MARK: - Error Handling
 
-public enum _MaybeAsyncProtocolError: Error {
+public enum _MaybeAsyncProtocolError: Equatable, Error {
+    case noResolutionImplementation
     case needsResolving
 }
