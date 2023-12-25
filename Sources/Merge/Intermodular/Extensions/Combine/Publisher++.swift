e13650cb6a00d38d3664eecd0510e1abec1d1499
Vatsal Manot <vatsal.manot@yahoo.com>
2023-12-25 01:05:51 -0800

    Update package

--- Sources/Merge/Intermodular/Extensions/Combine/Publisher++.swift
+++ Sources/Merge/Intermodular/Extensions/Combine/Publisher++.swift
@@ -7,6 +7,30 @@ import Foundation
 import Swift
 
 extension Publisher {
+    public func asyncSink(
+        receiveValue: @escaping (Output) -> Void
+    ) async throws {
+        let cancellable = SingleAssignmentAnyCancellable()
+        
+        try await withUnsafeThrowingContinuation { (continuation: UnsafeContinuation<Void, Error>) in
+            cancellable.set(
+                sink(
+                    receiveCompletion: { completion in
+                        switch completion {
+                            case .finished:
+                                continuation.resume(returning: ())
+                            case .failure(let failure):
+                                continuation.resume(throwing: failure)
+                        }
+                    },
+                    receiveValue: receiveValue
+                )
+            )
+        }
+    }
+}
+
+extension Publisher {
     /// Transforms all elements and errors from the upstream publisher to a `Result`.
     public func toResultPublisher() -> Publishers.Catch<Publishers.Map<Self, Result<Output, Failure>>, Just<Result<Output, Failure>>> {
         map(Result.success).catch {
