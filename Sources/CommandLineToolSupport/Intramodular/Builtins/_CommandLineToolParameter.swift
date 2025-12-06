6b8a311647339602cbb97ddf3a4bf96a6b4ffac8
Yanan Li <1474700628@qq.com>
2025-12-06 15:56:05 +0800

    Update API

--- Sources/CommandLineToolSupport/Intramodular/Builtins/_CommandLineToolParameter.swift
+++ Sources/CommandLineToolSupport/Intramodular/Builtins/_CommandLineToolParameter.swift
@@ -7,20 +7,20 @@ import Swallow
 
 public protocol _CommandLineToolParameterProtocol: PropertyWrapper {
     /// The name of the parameter as it will be passed in the actual command being invoked.
-    var name: _CommandLineToolParameterName { get }
+    var key: _CommandLineToolParameterOptionKey? { get }
     
     /// Defines how the parameter’s value is joined with its key when constructing the final command-line invocation.
     ///
     /// For example, `--output <path>`, or `--output=value`.
-    var valueStyle: _CommandLineToolParameterValueStyle { get }
+    var keyValueSeparator: _CommandLineToolParameterKeyValueSeparator? { get }
 }
 
 @propertyWrapper
 public struct _CommandLineToolParameter<WrappedValue>: _CommandLineToolParameterProtocol {
     var _wrappedValue: WrappedValue
 
-    public var name: _CommandLineToolParameterName
-    public var valueStyle: _CommandLineToolParameterValueStyle
+    public var key: _CommandLineToolParameterOptionKey?
+    public var keyValueSeparator: _CommandLineToolParameterKeyValueSeparator?
     
     public var wrappedValue: WrappedValue {
         get {
@@ -32,43 +32,47 @@ public struct _CommandLineToolParameter<WrappedValue>: _CommandLineToolParameter
     
     public init(
         wrappedValue: WrappedValue,
-        name: _CommandLineToolParameterName,
-        valueStyle: _CommandLineToolParameterValueStyle = .space
+        key: _CommandLineToolParameterOptionKey?,
+        keyValueSeparator: _CommandLineToolParameterKeyValueSeparator? = .space
     ) {
         self._wrappedValue = wrappedValue
-        self.name = name
-        self.valueStyle = valueStyle
+        self.key = key
+        self.keyValueSeparator = keyValueSeparator
     }
 }
 
-public enum _CommandLineToolParameterName: Hashable, Sendable {
+public enum _CommandLineToolParameterOptionKey: Hashable, Sendable {
     /// A parameter name prefixed with one hyphen, for example: `-o`, `-output`, etc.
     case hyphenPrefixed(String)
     /// A parameter name prefixed with two hyphens, for example: `--output`, etc.
     case doubleHyphenPrefixed(String)
+    /// A parameter name prefixed with a slash, for example: `/out`, etc.
+    ///
+    /// Commonly used in some Windows CLIs.
+    case slashPrefixed(String)
 }
 
-public enum _CommandLineToolParameterValueStyle: String, Hashable, Sendable {
-    /// A value style that use a space character as separator between key and value.
+public enum _CommandLineToolParameterKeyValueSeparator: String, Hashable, Sendable {
+    /// Uses a space character as separator between key and value.
     ///
     /// For example: `-o <path>`
     case space = " "
-    /// A value style that use an equal character as separator between key and value.
+    /// Uses an equal character as separator between key and value.
     ///
     /// For example: `-cxx-interoperability-mode=default` for `xcrun swiftc`
     case equal = "="
     
-    /// A value style that uses a plus character as separator between key and value.
+    /// Uses a plus character as separator between key and value.
     ///
     /// For example: `-framework+UIKit` for legacy `Id` CLT.
     ///
     /// - warning: This is a legacy value style and may not support in the mordern toolchain.
     case plus = "+"
     
-    /// A value style that uses a slash character as separator between key and value.
+    /// Uses a colon character as separator between key and value.
     ///
     /// Commonly used in some Windows CLIs, for example: `/out:program.exe`.
-    case slash = "/"
+    case colon = ":"
 }
 
 extension CommandLineTool {
