2fb8456876cf09b12a68874da25097d06907c21e
Yanan Li <1474700628@qq.com>
2025-12-06 14:20:04 +0800

    Add `_CommandLineToolParameterValueStyle`

--- Sources/CommandLineToolSupport/Intramodular/Builtins/_CommandLineToolParameter.swift
+++ Sources/CommandLineToolSupport/Intramodular/Builtins/_CommandLineToolParameter.swift
@@ -8,6 +8,11 @@ import Swallow
 public protocol _CommandLineToolParameterProtocol: PropertyWrapper {
     /// The name of the parameter as it will be passed in the actual command being invoked.
     var name: _CommandLineToolParameterName { get }
+    
+    /// Defines how the parameter’s value is joined with its key when constructing the final command-line invocation.
+    ///
+    /// For example, `--output <path>`, or `--output=value`.
+    var valueStyle: _CommandLineToolParameterValueStyle { get }
 }
 
 @propertyWrapper
@@ -15,6 +20,7 @@ public struct _CommandLineToolParameter<WrappedValue>: _CommandLineToolParameter
     var _wrappedValue: WrappedValue
 
     public var name: _CommandLineToolParameterName
+    public var valueStyle: _CommandLineToolParameterValueStyle
     
     public var wrappedValue: WrappedValue {
         get {
@@ -24,9 +30,14 @@ public struct _CommandLineToolParameter<WrappedValue>: _CommandLineToolParameter
         }
     }
     
-    public init(wrappedValue: WrappedValue, name: _CommandLineToolParameterName) {
+    public init(
+        wrappedValue: WrappedValue,
+        name: _CommandLineToolParameterName,
+        valueStyle: _CommandLineToolParameterValueStyle = .space
+    ) {
         self._wrappedValue = wrappedValue
         self.name = name
+        self.valueStyle = valueStyle
     }
 }
 
@@ -35,6 +46,29 @@ public enum _CommandLineToolParameterName: Hashable, Sendable {
     case hyphenPrefixed(String)
     /// A parameter name prefixed with two hyphens, for example: `--output`, etc.
     case doubleHyphenPrefixed(String)
+}
+
+public enum _CommandLineToolParameterValueStyle: String, Hashable, Sendable {
+    /// A value style that use a space character as separator between key and value.
+    ///
+    /// For example: `-o <path>`
+    case space = " "
+    /// A value style that use an equal character as separator between key and value.
+    ///
+    /// For example: `-cxx-interoperability-mode=default` for `xcrun swiftc`
+    case equal = "="
+    
+    /// A value style that uses a plus character as separator between key and value.
+    ///
+    /// For example: `-framework+UIKit` for legacy `Id` CLT.
+    ///
+    /// - warning: This is a legacy value style and may not support in the mordern toolchain.
+    case plus = "+"
+    
+    /// A value style that uses a slash character as separator between key and value.
+    ///
+    /// Commonly used in some Windows CLIs, for example: `/out:program.exe`.
+    case slash = "/"
 }
 
 extension CommandLineTool {
